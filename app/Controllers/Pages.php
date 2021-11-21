8a36ad5b3cefe7cbf39fc7e82bfc6201f912b6b3
Yanianggraeni <92706447+Yanianggraeni@users.noreply.github.com>
2021-11-21 09:46:19 +0700

    Pages.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -2,32 +2,32 @@
 
 namespace App\Controllers;
 
-use App\Models\....;
+use App\Models\MovieModel;
 
 class Pages extends BaseController
 {
     protected $movieModel;
-    public function __....()
+    public function __construct()
     {
-        $this->movieModel = new ....();
+        $this->movieModel = new MovieModel();
     }
 
     public function ....()
     {
-        $nowPlaying = $this->movieModel->where(.... , ....)->findAll();
-        $upComing = $this->movieModel->where(... , ....)->findAll();;
+        $nowPlaying = $this->movieModel->where('status' , 'now Playing')->findAll();
+        $upComing = $this->movieModel->where('status' , 'up Coming')->findAll();;
         $data = [
-            .... => $nowPlaying,
-            .... => $upComing
+            'now Playing' => $nowPlaying,
+            'up Coming' => $upComing
         ];
-        echo view('pages/home', ....);
+        echo view('pages/home', $data);
     }
 
-    public function ....($....)
+    public function movie($id_movie)
     {
-        $movie = $this->movieModel->where(.... , $....)->findAll();
+        $movie = $this->movieModel->where(id_movie , $id_movie)->findAll();
         $data = [
-            .... => $movie
+            'movie' => $movie
         ];
         echo view('pages/movie', $data);
     }
